c78637bca155f70c2f8066affc76c0bff867f6a6
llstrehl <127546794+llstrehl@users.noreply.github.com>
2023-11-07 20:10:49 -0300

    Add files via upload

---
+++ sketch.py
@@ -0,0 +1,12 @@
+# from anytree import Node, RenderTree
+
+while(True):
+    
+    expr = input().split(' ')
+    #separa expressão
+    
+    print(expr)
+    #teste
+    
+    # shunting-yard
+    # TODO
